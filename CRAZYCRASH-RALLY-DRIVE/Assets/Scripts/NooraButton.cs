ea3f73bacb091fdda455b7308163042ca7429d87
noorajurva <74291580+noorajurva@users.noreply.github.com>
2022-03-04 11:20:32 +0200

    Titlescreen lisätty

---
+++ CRAZYCRASH-RALLY-DRIVE/Assets/Scripts/NooraButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity;
+using UnityEngine.SceneManagement;
+
+public class NooraButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("koodi löytyy");
+    }
+
+    public void buttonPress()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+}
