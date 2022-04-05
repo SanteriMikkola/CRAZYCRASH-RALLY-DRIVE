44b18a9bf17917697ea2c63325b13c0751e6b3de
SanteriMikkola <santerimikkola04@gmail.com>
2022-04-05 15:03:28 +0300

    kerättävien itemien efekti tehty

---
+++ CRAZYCRASH-RALLY-DRIVE/Assets/Scripts/ItemsEffect.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsEffect : MonoBehaviour
+{
+
+    private Vector3 targetPos, upperPos,
+                    downPos;
+
+    private float speed = 0.15f;
+    public float rotSpeed;
+
+    public int randInt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        upperPos = new Vector3(transform.position.x, 1.15f, transform.position.z);
+        downPos = new Vector3(transform.position.x, 0.85f, transform.position.z);
+
+        targetPos = upperPos;
+
+        randInt = Random.Range(1, 11);
+
+        if (randInt >= 1 && randInt <= 5)
+        {
+            rotSpeed = -0.25f;
+        }
+        else
+        {
+            rotSpeed = 0.25f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == upperPos)
+        {
+            targetPos = downPos;
+        }
+        if (transform.position == downPos)
+        {
+            targetPos = upperPos;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        transform.Rotate(0f, rotSpeed, 0f);
+    }
+}
